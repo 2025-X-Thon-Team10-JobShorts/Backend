01cb855bf8582175a8b7b91264e54514f9e49860
kkimyy <96537275+kkimyy@users.noreply.github.com>
2025-11-23 04:35:25 +0900

    [FEAT] add bookmark and remove

---
+++ server/src/main/java/com/xhackathon/server/domain/bookmark/dto/request/BookmarkRequest.java
@@ -0,0 +1,9 @@
+package com.xhackathon.server.domain.bookmark.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class BookmarkRequest {
+    private String companyPid;
+    private Long shortFormId;
+}
